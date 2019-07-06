3981af82af5a1144fef09ad2b884dd2d27f182ad
Rhunterj <robbin_jagt@hotmail.com>
2019-07-06 22:49:24 +0200

    Api returns valid data

--- spotify-api/server/index.js
+++ spotify-api/server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+let request = require('request')
+let querystring = require('querystring')
+
+require('dotenv').config();
+console.log(process.env.SPOTIFY_CLIENT_ID);
+console.log(process.env.SPOTIFY_CLIENT_SECRET);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,6 +17,45 @@ app.get('/api/greeting', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+// Spotify api
+let redirect_uri =
+  process.env.REDIRECT_URI ||
+  'http://localhost:3001/callback';
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+});
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null;
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+
+  request.post(authOptions, function(error, response, body) {
+    let access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
--- spotify-api/src/App.js
+++ spotify-api/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {Component} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +12,60 @@ class App extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+      .then(data => this.setState({
+        user: {
+          name: data.display_name
+        }
+      }))
+
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+      .then(playlistData => {
+        let playlists = playlistData.items
+        let trackDataPromises = playlists.map(playlist => {
+          let responsePromise = fetch(playlist.tracks.href, {
+            headers: {'Authorization': 'Bearer ' + accessToken}
+          })
+          let trackDataPromise = responsePromise
+            .then(response => response.json())
+          return trackDataPromise
+        })
+        let allTracksDataPromises =
+          Promise.all(trackDataPromises)
+        let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+          trackDatas.forEach((trackData, i) => {
+            playlists[i].trackDatas = trackData.items
+              .map(item => item.track)
+              .map(trackData => ({
+                name: trackData.name,
+                duration: trackData.duration_ms / 1000
+              }))
+          })
+          return playlists
+        })
+        return playlistsPromise
+      })
+      .then(playlists => this.setState({
+        playlists: playlists.map(item => {
+          return {
+            name: item.name,
+            songs: item.trackDatas.slice(0,3)
+          }
+        })
+      }))
+
   }
 
   handleChange(event) {
@@ -26,6 +80,8 @@ class App extends Component {
   }
 
   render() {
+
+    console.log(this.state, this.props);
     return (
       <div className="App">
         <header className="App-header">
